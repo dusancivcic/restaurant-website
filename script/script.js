d26e39be3a79be6207571678dd3635ecb8d32183
dusancivcic <dusancivcic@gmail.com>
2021-04-10 16:10:03 +0200

    Updated

--- script/script.js
+++ script/script.js
@@ -6,4 +6,7 @@ window.onscroll = function(){
         btnTop.classList.remove('show')
     }
 }
-console.log('blablabla')
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  } 
